ca7e021c0cf64516f23f721ca9bfeb2fde8fd984
NikDoe <nikd0e@yandex.ru>
2022-09-12 15:49:49 +0300

    feature(pizza): starting server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hi there :)");
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = process.env.PORT || 9001;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const start = () => {
+	try {
+		app.listen(PORT, () => {
+			console.log(`сервер запущен на порту ${PORT}`);
+		});
+	} catch (e) {
+		console.log(e);
+	}
+};
+
+start();
