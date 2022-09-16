7b369ee2046e1f0490d0b54cbe3a9c7312bcc78e
NikDoe <nikd0e@yandex.ru>
2022-09-16 13:28:45 +0300

    feature(tutu): add train services

--- src/services/TrainService.js
+++ src/services/TrainService.js
@@ -1,15 +1,25 @@
-import { trainRepo } from '../utils/utils.js';
+import {ILike} from 'typeorm';
+import {trainRepo} from '../utils/utils.js';
 
 class TrainService {
-	async createTrain (train) {
+	async createTrain(train) {
 		const newTrain = trainRepo.create(train);
 		await trainRepo.save(newTrain);
 		return newTrain;
 	}
 
-	async allTrains ({ query }) {
+	async allTrains({searchFrom, searchTo}) {
 		let allTrains;
-		if (!query) allTrains = trainRepo.find();
+		if (!searchFrom && !searchTo) allTrains = trainRepo.find();
+		if (searchFrom && !searchTo) allTrains = trainRepo.find({
+			where: {depFrom: ILike(`%${searchFrom}%`)}
+		});
+		if (!searchFrom && searchTo) allTrains = trainRepo.find({
+			where: {arrivalTo: ILike(`%${searchTo}%`)}
+		});
+		if (searchFrom && searchTo) allTrains = trainRepo.find({
+			where: {depFrom: ILike(`%${searchFrom}%`), arrivalTo: ILike(`%${searchTo}%`)}
+		});
 		return allTrains;
 	}
 }
