a3f763dbdf7816a9d5722d81a0926f71cb5ec122
NikDoe <nikd0e@yandex.ru>
2022-09-19 13:23:04 +0300

    feature(tutu): add cors support

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
-import {AppDataSource} from "./db.js";
-import router from "./router/appRouter.js";
+import { AppDataSource } from './db.js';
+import router from './router/appRouter.js';
 
 const app = express();
 const PORT = process.env.PORT || 9001;
 
+const domainsFromEnv = process.env.CORS_DOMAINS || '';
+
+const whitelist = domainsFromEnv.split(',').map(item => item.trim());
+
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (!origin || whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+	credentials: true,
+};
+
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use('/api', router);
 
 const start = async () => {
@@ -18,7 +33,7 @@ const start = async () => {
 			.then(() => {
 				console.log('база данных подключена');
 			})
-			.catch((error) => console.log(error));
+			.catch(error => console.log(error));
 
 		app.listen(PORT, () => {
 			console.log(`сервер запущен на порту ${PORT}`);
