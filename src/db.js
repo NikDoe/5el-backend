5465db3441e368a705ef9b5f3be4e5387e834283
NikDoe <nikd0e@yandex.ru>
2022-09-19 11:54:17 +0300

    refactor(tutu): refactor booking model

--- src/db.js
+++ src/db.js
@@ -1,17 +1,17 @@
-import { DataSource } from 'typeorm';
+import {DataSource} from 'typeorm';
 import Train from './models/Train.js';
 import Booking from './models/Booking.js';
 
 export const AppDataSource = new DataSource({
-	type        : 'postgres',
-	url         : process.env.DATABASE_URL,
-	ssl         : {
-		rejectUnauthorized : false
+	type: 'postgres',
+	url: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
 	},
-	entities    : [
+	entities: [
 		Train,
 		Booking
 	],
-	synchronize : true,
-	logging     : false
+	synchronize: true,
+	logging: false
 });
--- src/models/Booking.js
+++ src/models/Booking.js
@@ -1,49 +1,49 @@
-import { EntitySchema } from 'typeorm';
+import {EntitySchema} from 'typeorm';
 
 export default new EntitySchema({
-	name      : 'Booking',
-	tableName : 'Bookings',
-	columns   : {
-		id           : {
-			primary   : true,
-			type      : 'int',
-			generated : true
+	name: 'Booking',
+	tableName: 'Bookings',
+	columns: {
+		id: {
+			primary: true,
+			type: 'int',
+			generated: true
 		},
-		title        : {
-			type : 'varchar'
+		title: {
+			type: 'varchar'
 		},
-		depFrom      : {
-			type : 'text'
+		from: {
+			type: 'text'
 		},
-		arrivalTo    : {
-			type : 'text'
+		to: {
+			type: 'text'
 		},
-		depTime      : {
-			type : 'time'
+		depTime: {
+			type: 'time'
 		},
-		arrivalTime  : {
-			type : 'time'
+		arrivalTime: {
+			type: 'time'
 		},
-		inTheWay     : {
-			type : 'time'
+		inTheWay: {
+			type: 'time'
 		},
-		seat         : {
-			type : 'text'
+		seat: {
+			type: 'text'
 		},
-		van          : {
-			type : 'text'
+		van: {
+			type: 'text'
 		},
-		price        : {
-			type : 'real'
+		price: {
+			type: 'real'
 		},
-		fio          : {
-			type : 'text'
+		fio: {
+			type: 'text'
 		},
-		passportData : {
-			type : 'text'
+		passportData: {
+			type: 'text'
 		},
-		phone        : {
-			type : 'text'
+		phone: {
+			type: 'text'
 		}
 	}
 });
